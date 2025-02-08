55ef56b957786708346ec05f19030387750ac0af
vaghasiyasahil <129865823+vaghasiyasahil@users.noreply.github.com>
2025-02-08 21:22:33 +0530

    new product update

--- updateproduct.php
+++ updateproduct.php
@@ -17,13 +17,15 @@
 	$price = $_POST['price'];
 	$description = $_POST['description'];
 	$imagedata = $_POST['imagedata'];
-	$imagename = $_POST['imagename'];
+	// $imagename = $_POST['imagename'];
 
 		$realimage = base64_decode($imagedata);
 
+		$imagename = "ProductImage/".$name.rand(0,10000).rand(0,10000).".jpg";
+
 		file_put_contents($imagename, $realimage);
 
-		$qry = "update products set PRO_NAME = '$name' , PRO_PRICE = '$price' , PRO_DES ='$description' where ID = '$id' ";
+		$qry = "update products set PRO_NAME = '$name' , PRO_PRICE = '$price' , PRO_DES ='$description' , PRO_IMAGE = '$imagename' where ID = '$id' ";
 
 		$sql = mysqli_query($con, $qry);
 
