e112aa4d73ef393dead7325ea1b02ef23adc7c26
vaghasiyasahil <sahilvaghasiya13@gmail.com>
2025-02-08 19:06:49 +0530

    Initial commit

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-    echo "Hello Words";
+    include_once("config.php");
+    $select="select * from Products";
+    $sql = mysqli_query($con, $qry);
+    while($row=mysqli_fetch_array($sql)){
+        echo $row['PRO_NAME'];
+        echo $row['PRO_DES'];
+    }
 ?>
