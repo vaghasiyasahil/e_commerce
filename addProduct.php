c5a3fd8cff850f73471bce91a8f78de9ea2389a5
vaghasiyasahil <sahilvaghasiya13@gmail.com>
2025-02-08 15:02:10 +0530

    Initial commit

---
+++ addProduct.php
@@ -0,0 +1,38 @@
+<?php 
+
+	include_once("config.php");
+   $temp   = array();
+
+   if($con)
+   {
+    $temp['connection']  = 1;
+   }
+   else 
+   {
+    $temp['connection']  = 0;
+   }
+	$loginid = $_POST['userid'];
+	$name = $_POST['pname'];
+	$price = $_POST['pprize'];
+	$description = $_POST['pdes'];
+	$imagedata = $_POST['productimage'];
+
+		$realimage = base64_decode($imagedata);
+
+		$imagename = "ProductImage/".$name.rand(0,10000).rand(0,10000).".jpg";
+
+		file_put_contents($imagename, $realimage);
+
+		$qry = "insert into Products(UID,PRO_NAME,PRO_DES,PRO_PRICE,PRO_IMAGE) values ('$loginid','$name','$description','$price','$imagename')";
+		
+		$sql = mysqli_query($con, $qry);
+		if($sql)
+		{
+			$temp['productaddd'] = 1;
+		}
+		else
+		{
+			$temp['productaddd'] = 0;
+		}
+	echo json_encode($temp);
+ ?>
---
+++ viewProduct.php
@@ -0,0 +1,50 @@
+<?php 
+	include_once("config.php");
+	$temp = array();
+
+	if($con)
+	{
+		$temp['connection'] = 1;
+	}
+	else
+	{
+		$temp['connection'] = 0;
+	}
+
+
+	$loginid = $_POST['userid'];
+
+
+		$qry = "select * from Products where UID = '$loginid'";
+
+		$sql = mysqli_query($con, $qry);
+
+		$cnt = mysqli_num_rows($sql);
+
+
+
+		if($cnt>0)
+		{
+			$temp['result'] = 1;
+
+			$productdata = array();
+			
+			while($arr = mysqli_fetch_assoc($sql)) {
+				
+				$productdata[] = $arr;
+			}
+
+			$temp['productdata'] = $productdata;
+
+
+		}
+		else
+		{
+			$temp['result'] = 0;
+		}
+	
+
+
+	echo json_encode($temp);
+
+ ?>
