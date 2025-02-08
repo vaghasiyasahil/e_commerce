5315a8ab837be7b0de9633f9e25f1fd60dd98096
vaghasiyasahil <sahilvaghasiya13@gmail.com>
2025-02-08 19:10:30 +0530

    Initial commit

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
     echo "Start";
-    include_once("config.php");
-    $select="select * from Products";
-    $sql = mysqli_query($con, $qry);
-    while($row=mysqli_fetch_array($sql)){
-        echo $row['PRO_NAME'];
-        echo $row['PRO_DES'];
+
+    try {
+        include_once("config.php");
+
+        // Enable Exception for MySQLi errors
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+        // Query execution
+        $select = "SELECT * FROM Products";
+        $sql = mysqli_query($con, $select);
+
+        // Fetch and display data
+        while ($row = mysqli_fetch_array($sql)) {
+            echo $row['PRO_NAME'] . "<br>";
+            echo $row['PRO_DES'] . "<br>";
+        }
+
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
     }
-    echo "end";
+
+    echo "End";
 ?>
