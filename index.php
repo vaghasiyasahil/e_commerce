e880d8580160247f823d2f969f667e6fbb2a5469
vaghasiyasahil <129865823+vaghasiyasahil@users.noreply.github.com>
2025-02-08 19:30:00 +0530

    update

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<?php
-    echo "Start";
-
-    try {
-        include_once("config.php");
-
-        // Query execution
-        $select = "SELECT * FROM usertable";
-        $sql = mysqli_query($con, $select);
-
-        // Fetch and display data
-        while ($row = mysqli_fetch_array($sql)) {
-            echo $row['name'] . "<br>";
-            echo $row['email'] . "<br>";
-        }
-
-        // Query execution
-        $select = "SELECT * FROM products";
-        $sql = mysqli_query($con, $select);
-
-        // Fetch and display data
-        while ($row = mysqli_fetch_array($sql)) {
-            echo $row['PRO_NAME'] . "<br>";
-            echo $row['PRO_DES'] . "<br>";
-        }
-
-    } catch (Exception $e) {
-        echo "<br>Error: " . $e->getMessage();
-    }
-
-    echo "<br>End";
-?>
+<?php 
+	echo "Welcome";
+ ?>
