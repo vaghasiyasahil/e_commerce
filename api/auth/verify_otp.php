09d85682ed5044280ab44f646f36383b9c6de306
Sahil Vaghasiya <129865823+vaghasiyasahil@users.noreply.github.com>
2025-06-13 22:01:54 +0530

    new line

--- api/auth/verify_otp.php
+++ api/auth/verify_otp.php
@@ -8,7 +8,7 @@ $userOtp = $data['otp'] ?? '';
 
 // Check if OTP cookie exists
 echo "store one otp = {$_COOKIE['otp']}";
-echo "<br>Email = {$_COOKIE['email']}";
+echo "\nEmail = {$_COOKIE['email']}";
 if (!isset($_COOKIE['otp']) || !isset($_COOKIE['email'])) {
     echo json_encode(["status" => "error", "message" => "OTP expired"]);
     exit;
@@ -18,8 +18,8 @@ $storedOtp = $_COOKIE['otp'];
 $storedEmail = $_COOKIE['email'];
 
 // Compare OTPs
-echo "<br>user otp = $userOtp";
-echo "<br>Store otp = $storedOtp";
+echo "\nuser otp = $userOtp";
+echo "\nStore otp = $storedOtp";
 if ($userOtp == $storedOtp) {
     
     // Optional: delete the cookie after successful verification
